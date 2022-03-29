f0f6d3eb32da68f82328646e0516b28f2808aac4
dollyllama <edolly2@protonmail.com>
2022-03-29 02:28:55 -0500

    ""

--- src/_components/Navigation.js
+++ src/_components/Navigation.js
@@ -2,17 +2,28 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+    <div className="top-nav">
+      <nav className="top-nav_nav">
+        <ul className="top-nav_list">
+          <li className="top-nav_list-item">HOME</li>
+          <li className="top-nav_list-item">
+            NEWS
+            {/* <ul className="top-nav_sub-list">
+              <li className="top-nav_list-item">LATEST</li>
+              <li className="top-nav_list-item">USA</li>
+              <li className="top-nav_list-item">WORLD</li>
+              <li className="top-nav_list-item">ARCHIVE</li>
+              <li className="top-nav_list-item">UNDER-C</li>
+              <li className="top-nav_list-item">UNDER-C</li>
+              <li className="top-nav_list-item">UNDER-C</li>
+              <li className="top-nav_list-item">UNDER-C</li>
+            </ul> */}
+          </li>
+          <li>FACT-CHECK</li>
+          <li>ACCOUNT</li>
+          <li>FOLLOW US</li>
+          <li>CONTACT</li>
+          <li>DONATE</li>
         </ul>
       </nav>
     </div>
