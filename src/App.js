05d2b54bbf0b54ee14e40ac680171025211f9aa1
dollyllama <edolly2@protonmail.com>
2022-03-29 21:21:05 -0500

    ""

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div>
       <Header />
-      <p>kadjf;aldkjf;aldskjfa;lkdsjfal;kdsjf;</p>
     </div>
   );
 }
--- src/_components/SiteBrand.js
+++ src/_components/SiteBrand.js
@@ -5,13 +5,7 @@ import logo from "../_assets/wild-boar-logo.png";
 const SiteBrand = () => {
   return (
     <div className="site-brand">
-      <img
-        src={logo}
-        className="App-logo"
-        alt="logo"
-        width="100"
-        height="auto"
-      />
+      <img src={logo} className="site-brand-logo" alt="logo" />
       <h2 className="site-brand-title">The Thwarthog</h2>
     </div>
   );
